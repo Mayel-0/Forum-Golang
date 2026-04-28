30896ff95ed8a64ddd108365548d0d9b29e02855
mael <llado.mael.33@gmail.com>
2026-04-28 12:49:15 +0200

    handlers add wait front

--- backend/handlers/poste.go
+++ backend/handlers/poste.go
@@ -1,15 +1,45 @@
 package handlers
 
-import "net/http"
+import (
+	"lyrics/auth"
+	"net/http"
+)
 
 func PosteCreateHandler(w http.ResponseWriter, r *http.Request) {
-
+	UserID, ok := auth.GetUserID(r)
+	if !ok {
+		http.Error(w, "Utilisateur non authentifié", http.StatusUnauthorized)
+		return
+	}
+	switch r.Method {
+	case http.MethodPost:
+	default:
+		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+	}
 }
 
 func PosteDeleteHandler(w http.ResponseWriter, r *http.Request) {
-
+	UserID, ok := auth.GetUserID(r)
+	if !ok {
+		http.Error(w, "Utilisateur non authentifié", http.StatusUnauthorized)
+		return
+	}
+	switch r.Method {
+	case http.MethodPost:
+	default:
+		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+	}
 }
 
 func PosteModifierHandle(w http.ResponseWriter, r *http.Request) {
-
+	UserID, ok := auth.GetUserID(r)
+	if !ok {
+		http.Error(w, "Utilisateur non authentifié", http.StatusUnauthorized)
+		return
+	}
+	switch r.Method {
+	case http.MethodPost:
+	default:
+		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
+	}
 }
