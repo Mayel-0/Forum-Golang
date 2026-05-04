82fb8b7c3ba306fe4f06a98dc1e8863e22d78302
mael <llado.mael.33@gmail.com>
2026-05-04 09:57:00 +0200

    add create post

--- backend/handlers/poste.go
+++ backend/handlers/poste.go
@@ -3,7 +3,10 @@ package handlers
 import (
 	"log"
 	"lyrics/auth"
+	"lyrics/models"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 func PosteCreateHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +17,18 @@ func PosteCreateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	switch r.Method {
 	case http.MethodPost:
-		log.Println(UserID)
+		userIDParsed, err := uuid.Parse(UserID)
+		if err != nil {
+			http.Error(w, "ID utilisateur invalide", http.StatusBadRequest)
+			return
+		}
+
+		Post := models.Post{
+			AuthorID: userIDParsed,
+			Title:    r.FormValue("title"),
+			Body:     r.FormValue("body"),
+		}
+		log.Printf("Nouveau post: %+v", Post)
 	default:
 		http.Error(w, "Méthode non autorisée", http.StatusMethodNotAllowed)
 	}
