c4c6d6bb5b0abdf90c28a66f4fccfdba76efb4cc
mael <llado.mael.33@gmail.com>
2026-05-05 12:25:12 +0200

    add compte like comment and post and starts commander handlers

--- backend/repositories/comment.go
+++ backend/repositories/comment.go
@@ -1,1 +1,38 @@
 package repositories
+
+import (
+	"errors"
+	dbpkg "lyrics/db"
+	"lyrics/models"
+
+	"github.com/google/uuid"
+)
+
+func AddComment(comment *models.Comment) error {
+	if dbpkg.Db == nil {
+		return errors.New("db not initialized")
+	}
+
+	return dbpkg.Db.Create(comment).Error
+}
+
+func DeleteComment(commentID uuid.UUID) error {
+	if dbpkg.Db == nil {
+		return errors.New("db not initialized")
+	}
+
+	return dbpkg.Db.Delete(&models.Comment{}, commentID).Error
+}
+
+func GetCommentsByPostID(postID uuid.UUID) ([]models.Comment, error) {
+	if dbpkg.Db == nil {
+		return nil, errors.New("db not initialized")
+	}
+
+	var comments []models.Comment
+	if err := dbpkg.Db.Where("post_id = ?", postID).Order("created_at ASC").Find(&comments).Error; err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
